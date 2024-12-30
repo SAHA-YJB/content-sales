be7499a303d27afd71521e00a531428a3caa905b
SAHA-YJB <pkomom123@naver.com>
2024-12-30 17:00:17 +0900

    네브바 및 반응형

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Navigation from '@/components/common/Navigation';
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className='row-start-2 mx-auto flex w-full flex-col items-center gap-4 px-2 pt-2 sm:items-start lg:max-w-4xl lg:gap-8 lg:pt-8'>
+      <Navigation />
+
+      <div className='h-[2000px] w-full bg-red-400'>body</div>
+    </main>
+  );
 }
---
+++ src/components/common/Navigation.tsx
@@ -0,0 +1,12 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+export default function Navigation() {
+  return (
+    <Tabs defaultValue='products' className='sticky top-0 z-50 w-full'>
+      <TabsList className='grid w-full grid-cols-2'>
+        <TabsTrigger value='products'>상품</TabsTrigger>
+        <TabsTrigger value='management'>관리</TabsTrigger>
+      </TabsList>
+    </Tabs>
+  );
+}
